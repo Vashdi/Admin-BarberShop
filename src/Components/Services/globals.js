6d0b9c681099ce85bac24f1e86b32490197841ad
Vashdi <62853009+Vashdi@users.noreply.github.com>
2021-10-17 01:35:27 +0300

    blah

--- src/Components/Services/globals.js
+++ src/Components/Services/globals.js
@@ -1,14 +1,14 @@
 const urls = {
-    appointments: "http://localhost:3001/appointments",
-    adminAppointments: "http://localhost:3001/adminAppointment",
-    adminLogin: "http://localhost:3001/adminLogin",
-    closedDays: "http://localhost:3001/closedDays",
-    break: "http://localhost:3001/adminAppointment/break",
-    auth: "http://localhost:3001/authorization",
-    strict: "http://localhost:3001/strict",
-    strictDay: "http://localhost:3001/strictDay",
-    users: "http://localhost:3001/users",
-    blackList: "http://localhost:3001/blackList"
+    appointments: "https://snir-barbershop.herokuapp.com/appointments",
+    adminAppointments: "https://snir-barbershop.herokuapp.com/adminAppointment",
+    adminLogin: "https://snir-barbershop.herokuapp.com/adminLogin",
+    closedDays: "https://snir-barbershop.herokuapp.com/closedDays",
+    break: "https://snir-barbershop.herokuapp.com/adminAppointment/break",
+    auth: "https://snir-barbershop.herokuapp.com/authorization",
+    strict: "https://snir-barbershop.herokuapp.com/strict",
+    strictDay: "https://snir-barbershop.herokuapp.com/strictDay",
+    users: "https://snir-barbershop.herokuapp.com/users",
+    blackList: "https://snir-barbershop.herokuapp.com/blackList"
 };
 
 export default urls;
